8e6b3d7afdb7d8d34180b6d8e4bd879c43e41d8c
ouafae <ouafae.benkadi@gmail.com>
2020-10-08 20:07:14 +0200

    add version model

---
+++ sdk-core/src/main/java/care/data4life/sdk/network/model/Version.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2020 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+public class Version {
+
+    @Json(name = "versions")
+    private int versionCode;
+    @Json(name = "version_name")
+    private String versionName;
+    @Json(name = "status")
+    private String status;
+
+    public static String KEY_DEPRECATED = "deprecated";
+    public static String KEY_UNSUPPORTED = "unsupported";
+
+    public Version(int versionCode) {
+        this.versionCode = versionCode;
+        this.versionName = "";
+        this.status = KEY_UNSUPPORTED;
+    }
+
+    public int getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(int versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

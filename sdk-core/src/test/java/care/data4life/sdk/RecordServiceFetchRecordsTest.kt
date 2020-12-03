d9346122eb9be473132890830d42bdc9cbc35528
Matthias Geisler <matthias.geisler@data4life.care>
2020-12-03 17:42:19 +0100

    Refactor RecordServiceTest#fetchRecords*

---
+++ sdk-core/src/test/java/care/data4life/sdk/RecordServiceFetchRecordsTest.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2020 D4L data4life gGmbH / All rights reserved.
+ *
+ * D4L owns all legal rights, title and interest in and to the Software Development Kit ("SDK"),
+ * including any intellectual property rights that subsist in the SDK.
+ *
+ * The SDK and its documentation may be accessed and used for viewing/review purposes only.
+ * Any usage of the SDK for other purposes, including usage for the development of
+ * applications/third-party applications shall require the conclusion of a license agreement
+ * between you and D4L.
+ *
+ * If you are interested in licensing the SDK for your own applications/third-party
+ * applications and/or if you’d like to contribute to the development of the SDK, please
+ * contact D4L by email to help@data4life.care.
+ */
+
+package care.data4life.sdk
+
+import care.data4life.fhir.stu3.model.CarePlan
+import care.data4life.fhir.stu3.model.DomainResource
+import care.data4life.sdk.lang.DataValidationException
+import com.google.common.truth.Truth
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import java.io.IOException
+import java.util.*
+
+class RecordServiceFetchRecordsTest: RecordServiceTestBase() {
+    @Before
+    fun setUp() {
+        init()
+    }
+
+    @Test
+    @Throws(InterruptedException::class, IOException::class, DataValidationException.ModelVersionNotSupported::class)
+    fun fetchRecord_shouldReturnFetchedRecord() {
+        // Given
+        Mockito.`when`(mockApiService.fetchRecord(ALIAS, USER_ID, RECORD_ID)).thenReturn(Single.just(mockEncryptedRecord))
+        Mockito.doReturn(mockDecryptedRecord).`when`(recordService).decryptRecord<DomainResource>(mockEncryptedRecord, USER_ID)
+        Mockito.doReturn(mockMeta).`when`(recordService).buildMeta(mockDecryptedRecord)
+
+        // When
+        val observer = recordService.fetchRecord<CarePlan>(RECORD_ID, USER_ID).test().await()
+
+        // Then
+        val record = observer
+                .assertNoErrors()
+                .assertComplete()
+                .assertValueCount(1)
+                .values()[0]
+        Truth.assertThat(record.meta).isEqualTo(mockMeta)
+        Truth.assertThat(record.fhirResource).isEqualTo(mockCarePlan)
+        inOrder.verify(mockApiService).fetchRecord(ALIAS, USER_ID, RECORD_ID)
+        inOrder.verify(recordService).decryptRecord<DomainResource>(mockEncryptedRecord, USER_ID)
+        inOrder.verify(recordService).buildMeta(mockDecryptedRecord)
+        inOrder.verifyNoMoreInteractions()
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun fetchRecordsByUserIds_shouldReturnFetchedRecords() {
+        // Given
+        Mockito.doReturn(Single.just(mockRecord)).`when`(recordService).fetchRecord<DomainResource>(RECORD_ID, USER_ID)
+        val ids = Arrays.asList(RECORD_ID, RECORD_ID)
+
+        // When
+        val observer = recordService.fetchRecords<CarePlan>(ids, USER_ID).test().await()
+
+        // Then
+        val result = observer
+                .assertNoErrors()
+                .assertComplete()
+                .assertValueCount(1)
+                .values()[0]
+        Truth.assertThat(result.successfulFetches).hasSize(2)
+        Truth.assertThat(result.failedFetches).hasSize(0)
+        inOrder.verify(recordService).fetchRecords<DomainResource>(ids, USER_ID)
+        inOrder.verify(recordService, Mockito.times(2)).fetchRecord<DomainResource>(RECORD_ID, USER_ID)
+        inOrder.verifyNoMoreInteractions()
+    }
+
+    @Test
+    @Throws(InterruptedException::class, IOException::class, DataValidationException.ModelVersionNotSupported::class)
+    fun fetchRecordsByResourceType_shouldReturnFetchedRecords() {
+        // Given
+        val encryptedRecords = Arrays.asList(mockEncryptedRecord, mockEncryptedRecord)
+        Mockito.`when`(mockTaggingService.getTagFromType(CarePlan.resourceType)).thenReturn(mockTags)
+        Mockito.`when`(mockTagEncryptionService.encryptTags(mockTags)).thenReturn(mockEncryptedTags)
+        Mockito.`when`(mockApiService.fetchRecords(ArgumentMatchers.eq(ALIAS), ArgumentMatchers.eq(USER_ID), ArgumentMatchers.isNull(), ArgumentMatchers.isNull(), ArgumentMatchers.eq(10), ArgumentMatchers.eq(0), ArgumentMatchers.eq(mockEncryptedTags))).thenReturn(Observable.just(encryptedRecords))
+        Mockito.doReturn(mockDecryptedRecord).`when`(recordService).decryptRecord<DomainResource>(mockEncryptedRecord, USER_ID)
+        Mockito.doReturn(mockMeta).`when`(recordService).buildMeta(mockDecryptedRecord)
+
+        // When
+        val observer = recordService.fetchRecords(
+                USER_ID,
+                CarePlan::class.java,
+                null,
+                null,
+                10,
+                0).test().await()
+
+        // Then
+        val fetched = observer
+                .assertNoErrors()
+                .assertComplete()
+                .assertValueCount(1)
+                .values()[0]
+        Truth.assertThat(fetched).hasSize(2)
+        Truth.assertThat(fetched[0].meta).isEqualTo(mockMeta)
+        Truth.assertThat(fetched[0].fhirResource).isEqualTo(mockCarePlan)
+        Truth.assertThat(fetched[1].meta).isEqualTo(mockMeta)
+        Truth.assertThat(fetched[1].fhirResource).isEqualTo(mockCarePlan)
+        inOrder.verify(mockTaggingService).getTagFromType(CarePlan.resourceType)
+        inOrder.verify(mockTagEncryptionService).encryptTags(mockTags)
+        inOrder.verify(mockApiService).fetchRecords(ArgumentMatchers.eq(ALIAS), ArgumentMatchers.eq(USER_ID), ArgumentMatchers.isNull(), ArgumentMatchers.isNull(), ArgumentMatchers.eq(10), ArgumentMatchers.eq(0), ArgumentMatchers.eq(mockEncryptedTags))
+        inOrder.verify(recordService).decryptRecord<DomainResource>(mockEncryptedRecord, USER_ID)
+        inOrder.verify(recordService).buildMeta(mockDecryptedRecord)
+        inOrder.verify(recordService).decryptRecord<DomainResource>(mockEncryptedRecord, USER_ID)
+        inOrder.verify(recordService).buildMeta(mockDecryptedRecord)
+        inOrder.verifyNoMoreInteractions()
+    }
+}

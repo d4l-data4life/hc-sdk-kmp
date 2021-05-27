8bb495ac081df6862deb0f4cdde5451941601a9a
Matthias Geisler <matthias.geisler@data4life.care>
2021-05-27 08:12:51 +0200

    Enable count for arbitrary data in RecordService

--- sdk-core/src-gen/main/java/care/data4life/sdk/config/SDKConfig.kt
+++ sdk-core/src-gen/main/java/care/data4life/sdk/config/SDKConfig.kt
@@ -17,5 +17,5 @@
 package care.data4life.sdk.config
 
 object SDKConfig {
-    const val version: String = "1.12.0-change-resource-client-tests-SNAPSHOT"
+    const val version: String = "1.12.0-change-resource-clients-SNAPSHOT"
 }

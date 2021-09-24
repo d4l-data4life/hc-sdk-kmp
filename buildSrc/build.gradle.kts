7f34798e38b969befb3b58601e821dddaede1337
Matthias Geisler <matthias.geisler@data4life.care>
2021-09-24 17:25:34 +0200

    Bump buildSrc dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -44,14 +44,14 @@ dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
 
     // dependency check
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     // download scripts
-    implementation("de.undercouch:gradle-download-task:4.1.1")
+    implementation("de.undercouch:gradle-download-task:4.1.2")
     // publishing.gradle.kts
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.11.0.202103091610-r")
     // quality.gradle.kts
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.10.2")
-    implementation("com.pinterest:ktlint:0.41.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.14.3")
+    implementation("com.pinterest:ktlint:0.42.1")
     // versioning.gradle.kts
     implementation("care.data4life.gradle.gitversion:gradle-git-version:0.12.4-d4l")
 }
